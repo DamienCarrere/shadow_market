676307791b7705077d8fa1977b78fe79ca75769b
Aidesu <waoxsan@gmail.com>
2025-08-23 01:54:25 +0200

    Working on home page mobile

---
+++ app.js
@@ -0,0 +1,5 @@
+import { products } from "./ressources/js/homeProducts.js";
+import { search } from "./ressources/js/searchBar.js";
+
+products()
+search()
---
+++ ressources/js/homeProducts.js
@@ -0,0 +1,26 @@
+export function products() {
+    const dealsForYou = document.querySelector(".dealsForYou")
+
+fetch('https://dummyjson.com/products')
+    .then((res) => res.json())
+    .then((data) => {
+        console.log(data)
+        data.products.forEach(element => {
+            const productCard = document.createElement("div");
+            const pictureDiv = document.createElement("div");
+            const productImg = document.createElement("img");
+            const productName = document.createElement("h3");
+            const ProductPrice = document.createElement("h4");
+
+            dealsForYou.appendChild(productCard)
+            productCard.append(pictureDiv, productName, ProductPrice)
+            pictureDiv.appendChild(productImg);
+
+            productCard.classList.add("productsCard")
+            pictureDiv.classList.add("pictureCard")
+            productImg.src = element.thumbnail;
+            productName.textContent = element.title;
+            ProductPrice.textContent = `$${element.price}`;
+        });
+    })
+}
