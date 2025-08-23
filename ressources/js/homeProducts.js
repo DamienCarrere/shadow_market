fe83c7b367539c41991af3dcc6e2527bf53fb95d
Aidesu <waoxsan@gmail.com>
2025-08-23 10:23:14 +0200

    Adding button products

--- ressources/js/homeProducts.js
+++ ressources/js/homeProducts.js
@@ -8,19 +8,25 @@ fetch('https://dummyjson.com/products')
         data.products.forEach(element => {
             const productCard = document.createElement("div");
             const pictureDiv = document.createElement("div");
+            const productBtn = document.createElement("button");
             const productImg = document.createElement("img");
             const productName = document.createElement("h3");
             const ProductPrice = document.createElement("h4");
 
             dealsForYou.appendChild(productCard)
-            productCard.append(pictureDiv, productName, ProductPrice)
+            productCard.append(pictureDiv, productBtn, productName, ProductPrice)
             pictureDiv.appendChild(productImg);
 
             productCard.classList.add("productsCard")
             pictureDiv.classList.add("pictureCard")
+            productBtn.id = "productButton";
             productImg.src = element.thumbnail;
             productName.textContent = element.title;
             ProductPrice.textContent = `$${element.price}`;
+
+            productBtn.addEventListener("click", () => {
+                console.log("UwU");
+            })
         });
     })
 }
