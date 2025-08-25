7893fb3b2a035fc1b6e65ff4eac3ee0552d3aead
Eszter112 <balieszter1212@gmail.com>
2025-08-25 14:09:24 +0200

    correction apres Damien

--- checkout.js
+++ checkout.js
@@ -1,50 +1,52 @@
 // validations coordonnées
 
 function validateForm() {
-  // champs obligatoire
-  const mandatoryForm = [
-    "email",
-    "firstName",
-    "lastName",
-    "address",
-    "city",
-    "country",
-    "postal",
-    "card",
-    "nameOnCard",
-    "expiration",
-    "cvv",
-  ];
+    // champs obligatoire
+    const mandatoryForm = [
+        "email",
+        "firstName",
+        "lastName",
+        "address",
+        "city",
+        "country",
+        "postal",
+        "card",
+        "nameOnCard",
+        "expiration",
+        "cvv",
+    ];
 
-  // Suppose que tout est valable
-  let valid = true;
+    // Suppose que tout est valable
+    let valid = true;
 
-  //Parcour tous les champs obligatoire
-  mandatoryForm.forEach(function (fieldName) {
-    console.log("test");
+    //Parcour tous les champs obligatoire
+    mandatoryForm.forEach(function (fieldName) {
+        console.log("test", fieldName);
 
-    // chercher le champ donne le HTML
-    const inputElement = document.querySelector(`[name="${fieldName}"]`);
+        // chercher le champ donne le HTML
+        const inputElement = document.querySelector(`[name="${fieldName}"]`);
 
-    // si le champe n'est pas trouvé ou son contenu est vide
-    if (!inputElement || inputElement.value.trim() === "") {
-      // => error
-      inputElement.classList.add("error");
+        // si le champe n'est pas trouvé ou son contenu est vide
+        if (!inputElement || inputElement.value.trim() === "") {
+            // => error
+            inputElement.classList.add("error");
 
-      // la forme nest pas valide
-      valid = false;
-    } else {
-      // si ok, j'enleve error
-      inputElement.classList.remove("error");
+            // la forme nest pas valide
+            valid = false;
+        } else {
+            // si ok, j'enleve error
+            inputElement.classList.remove("error");
+        }
+    });
+
+    // si l'utilisateur a accepté les condotions...
+    const termsCheckbox = document.querySelector("#terms");
+    if (!termsCheckbox.checked) {
+        alert("You must accept the terms of purchase");
+        valid = false;
     }
-  });
 
-  // si l'utilisateur a accepté les condotions...
-  const termsCheckbox = document.querySelector("#terms");
-  if (!termsCheckbox.checked) {
-    alert("You must accept the terms of purchase");
-    valid = false;
-  }
-
-  return valid;
+    return valid;
 }
+
+//choix livraison
